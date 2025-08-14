5480fe7dbccf4f2390cae35f8bebcca73b3300c5
Aqib-Ayoub <aqibayoub321@gmail.com>
2025-08-14 10:01:39 +0530

    added splash screen and much more

---
+++ lib/views/on_boarding/splash_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class StartupView extends StatefulWidget {
+  const StartupView({super.key});
+
+  @override
+  State<StartupView> createState() => _StartupViewState();
+}
+
+class _StartupViewState extends State<StartupView> {
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/splash_bg.png',
+            width: media.width,
+            height: media.height,
+            fit: BoxFit.cover,
+          ),
+          Image.asset(
+            'assets/images/app_logo.png',
+            width: media.width * 0.7,
+            height: media.height * 0.7,
+            fit: BoxFit.contain,
+          ),
+        ],
+      ),
+    );
+  }
+}
