634157e9d77cc571ddabda70d32e215c2cca41fa
Aqib-Ayoub <aqibayoub321@gmail.com>
2025-08-16 21:27:54 +0530

    created new password screen otp screen

--- lib/views/auth/new_password_view.dart
+++ lib/views/auth/new_password_view.dart
@@ -1,18 +1,19 @@
 import 'package:client/common_widget/round_button.dart';
 import 'package:client/common_widget/round_textfield.dart';
 import 'package:client/const/color_extension.dart';
-import 'package:client/views/auth/new_password_view.dart';
+import 'package:client/views/auth/otp_view.dart';
 import 'package:flutter/material.dart';
 
-class ResetPasswordView extends StatefulWidget {
-  const ResetPasswordView({super.key});
+class NewPasswordView extends StatefulWidget {
+  const NewPasswordView({super.key});
 
   @override
-  State<ResetPasswordView> createState() => _ResetPasswordViewState();
+  State<NewPasswordView> createState() => _NewPasswordViewState();
 }
 
-class _ResetPasswordViewState extends State<ResetPasswordView> {
-  TextEditingController forgetEmail = TextEditingController();
+class _NewPasswordViewState extends State<NewPasswordView> {
+  TextEditingController newPasword = TextEditingController();
+  TextEditingController confirmPasword = TextEditingController();
   @override
   Widget build(BuildContext context) {
     var media = MediaQuery.of(context).size;
@@ -23,32 +24,35 @@ class _ResetPasswordViewState extends State<ResetPasswordView> {
           children: [
             SizedBox(height: 75.0),
             Text(
-              'Reset Password',
+              'New Password',
               style: TextStyle(
-                fontSize: 30,
                 color: AColor.primaryText,
-                fontWeight: FontWeight.w800,
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
               ),
             ),
             SizedBox(height: 10),
             Text(
-              textAlign: TextAlign.center,
-              'Please enter your email to recive a \nlink to create a new password via email',
+              'Please enter your password',
               style: TextStyle(
-                fontSize: 14,
                 color: AColor.primaryText,
                 fontWeight: FontWeight.w500,
               ),
             ),
-            SizedBox(height: 30),
-            RoundTextfield(hintText: 'Your Email', controller: forgetEmail),
+            SizedBox(height: 30.0),
+            RoundTextfield(hintText: 'New Password', controller: newPasword),
+            SizedBox(height: 30.0),
+            RoundTextfield(
+              hintText: 'Confirm Password',
+              controller: confirmPasword,
+            ),
             SizedBox(height: 30.0),
             RoundButton(
-              text: 'Send',
+              text: 'Next',
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => NewPasswordView()),
+                  MaterialPageRoute(builder: (context) => OtpView()),
                 );
               },
             ),
