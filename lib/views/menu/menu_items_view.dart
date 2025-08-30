c093e433d7e075d43607de0c0fae360ad0f6aa26
Aqib-Ayoub <aqibayoub321@gmail.com>
2025-08-30 20:51:23 +0530

    added menu items view

---
+++ lib/views/menu/menu_items_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MenuItemsView extends StatefulWidget {
+  const MenuItemsView({super.key});
+
+  @override
+  State<MenuItemsView> createState() => _MenuItemsViewState();
+}
+
+class _MenuItemsViewState extends State<MenuItemsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
